bfa789aee5daf863ab3ddecefe7ef9fe1d1c1d27
EljayDev <eljaydev01@gmail.com>
2022-09-09 21:50:47 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	{
 		putchar(n);
 		if (n != 57)
-														{
-															putchar(',');
-															putchar(' ');
-														}
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
